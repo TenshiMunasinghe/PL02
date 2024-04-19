39dba3ff4558869035e2d4dfb056741c5b24d878
TenshiMunasinghe <shadowghost1007@gmail.com>
2024-04-19 11:58:38 +0900

    wip

---
+++ handson3/Date.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct {
+  // Complete definition according
+  // to hands-on materials
+  // ...
+  int year;
+  int month;
+  int day;
+} Date;
+
+int readDate(FILE *in, Date *pDate);
+bool isDateLegal(const Date *pDate);
+void printDate(FILE *out, const Date *pDate);
+void printDateLine(FILE *out, const Date *pDate);
+int getAge(const Date *pDate, const Date *pCurrent);
---
+++ handson3/String_Oper.c
@@ -0,0 +1,57 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#include "String_Oper.h"
+
+// Names are words containing only letters
+int getName(FILE *in, char *name, int limit) {
+  char ch = fgetc(in);
+  int ixLit = 0;
+
+  // Skip leading spaces
+  while (isspace(ch))
+    ch = fgetc(in);
+  if (ch == EOF)
+    return EOF;
+  // Reading a word of letters
+  while (!isspace(ch) && ch != EOF) {
+    if (!isalpha(ch))
+      return 0;
+
+    if (ixLit < limit)
+      name[ixLit++] = ch;
+
+    ch = fgetc(in);
+  }
+  name[ixLit] = '\0';
+
+  return 1;
+}
+
+// IDs are words contaning letters and digits
+int getId(FILE *in, char *id, int limit) {
+  // Complete this function definition
+  // similar to getName()
+  // ...
+  char ch = fgetc(in);
+  int ixLit = 0;
+
+  // Skip leading spaces
+  while (isspace(ch))
+    ch = fgetc(in);
+  if (ch == EOF)
+    return EOF;
+  // Reading a word of letters
+  while (!isspace(ch) && ch != EOF) {
+    if (!isalpha(ch) && !isdigit(ch))
+      return 0;
+
+    if (ixLit < limit)
+      id[ixLit++] = ch;
+
+    ch = fgetc(in);
+  }
+  id[ixLit] = '\0';
+
+  return 1;
+}
