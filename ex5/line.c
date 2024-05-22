44cb911d5cfc182f64d0fe65c72b00487409322c
TenshiMunasinghe <shadowghost1007@gmail.com>
2024-05-22 15:43:17 +0900

    wip

---
+++ ex5/line.c
@@ -0,0 +1,5 @@
+#include "line.h"
+
+int computeLineFromPoints(Line line, Point p1, Point p2)
+{
+}
---
+++ ex5/line.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "point.h"
+
+typedef struct
+{
+  double gradient;
+  double intercept;
+
+} Line;
